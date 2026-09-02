7e083d62101625080d8f656c1f6c6538c7c3fc6a
전승현 <jeonseunghyeon@jeonseunghyeon-ui-MacBookPro.local>
2026-09-02 12:08:54 +0900

    fix:User Id타입 테스트 용이하게 long으로 변경 및 event엔티티생성

--- src/main/java/com/seunghyeon/seat_lock/entity/event.java
+++ src/main/java/com/seunghyeon/seat_lock/entity/event.java
@@ -4,19 +4,22 @@ package com.seunghyeon.seat_lock.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
 
-
-@Table(name = "users")
+@Table(name = "events")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
-public class User {
+public class event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column
     private String name;
+
+    @Column
+    private Instant startedAt;
 }
